5e04a4c3a4f67166d878d05eab8e76fba5b048be
lorenaalmeida2026 <lorenaalmeida19@gmail.com>
2026-08-16 12:30:03 -0300

    Update programaDados.c

--- programaDados.c
+++ programaDados.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+//declaracao de variaveis
+
+int main(){
+
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Informe sua matricula: \n");
+    scanf("%d", &matricula);
+
+    printf("informe sua altura: \n");
+    scanf("%.2f", &altura);
+
+    printf("Informe o seu nome: \n");
+    scanf("%s", &nome);
+
+    printf("Nome do Aluno: %s - Matricula: %d \n"n nome, matricula);
+    printf("idade do aluno: %d \n", idade);
+    printf("Altura do aluno: %.2f \n", altura);
+
+    returno 0;
+
+}
